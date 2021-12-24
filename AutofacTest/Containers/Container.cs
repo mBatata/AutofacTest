48baa46c6191510613eb674867a871f926e39c0e
Marco António <marco_antonio_099@hotmail.com>
2021-12-24 19:26:44 +0000

    Added Build Container Registry

---
+++ AutofacTest/Containers/Container.cs
@@ -0,0 +1,19 @@
+﻿using Autofac;
+using AutofacTest.Classes;
+using AutofacTest.Interfaces;
+
+namespace AutofacTest.Containers
+{
+    public static class Container
+    {
+        public static void RegisterContainer()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterType<ConsoleOutput>().As<IOutput>();
+            builder.RegisterType<TodayWriter>().As<IDateWriter>();
+
+            builder.Build();
+        }
+    }
+}
